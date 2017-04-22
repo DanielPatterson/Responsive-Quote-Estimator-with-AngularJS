7676a406504db814c9b6e14c21b056573f0daab5
Daniel Patterson <danieljpatterson77@gmail.com>
2017-04-23 00:23:45 +1000

    Add files via upload

---
+++ OrderFormController.js
@@ -0,0 +1,74 @@
+function OrderFormController($scope){
+
+	$scope.items = [
+		{
+			name: 'Social Media Campaign',
+			desc: 'loreum ipsum dolar sit amet',
+			price: 8000, // to change price change this number
+			active: false
+		},
+		{
+			name: 'Search Engine Optimsiation',
+			desc: 'loreum ipsum dolar sit amet',
+			price: 10000, // to change price change this number
+			active: false
+		},
+		{
+			name: 'CMS Intergration',
+			desc: 'loreum ipsum dolar sit amet',
+			price: 15000, // to change price change this number
+			active: false
+		},
+		{
+			name: 'User Testing',
+			desc: 'loreum ipsum dolar sit amet',
+			price: 20000, // to change price change this number
+			active: false
+		},
+		{
+			name: 'Accessibility Audit',
+			desc: 'loreum ipsum dolar sit amet',
+			price: 25000, // to change price change this number
+			active: false
+		},
+		{
+			name: 'Usability Audit',
+			desc: 'loreum ipsum dolar sit amet',
+			price: 30000, // to change price change this number
+			active: false
+		},
+		{
+			name: 'Design',
+			desc: 'loreum ipsum dolar sit amet',
+			price: 40000, // to change price change this number
+			active: false
+		},
+		{
+			name: 'Development',
+			desc: 'loreum ipsum dolar sit amet',
+			price: 60000, // to change price change this number
+			active: false
+		}
+		 
+	];
+	
+
+	$scope.toggleActive = function(s){
+		s.active = !s.active;
+	};
+
+	$scope.total = function(){
+
+		var total = 0;
+
+		angular.forEach($scope.items, function(s){
+			if (s.active){
+				total+= s.price;
+			}
+		});
+		
+		return total;
+	};
+
+	
+}
